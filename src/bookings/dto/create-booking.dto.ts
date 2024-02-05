817b4dafb0cf32dbe4305951cbf9cd14fcb41596
waseemansar <waseemansar@outlook.com>
2024-02-05 11:17:20 +0400

    feat: added validation to create booking dto using class validator

--- src/bookings/dto/create-booking.dto.ts
+++ src/bookings/dto/create-booking.dto.ts
@@ -1,1 +1,52 @@
-export class CreateBookingDto {}
+import { Currency } from '@prisma/client';
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsPositive, IsString, IsUrl, Min } from 'class-validator';
+
+export class CreateBookingDto {
+    @IsOptional()
+    @IsInt()
+    @Min(0)
+    bookingId?: number;
+
+    @IsOptional()
+    @IsInt()
+    @Min(0)
+    propertyId?: number;
+
+    @IsNotEmpty()
+    @IsString()
+    propertyName: string;
+
+    @IsUrl()
+    propertyImageUrl: string;
+
+    @IsNotEmpty()
+    @IsString()
+    arrivalDate: string;
+
+    @IsNotEmpty()
+    @IsString()
+    departureDate: string;
+
+    @IsPositive()
+    totalGuests: number;
+
+    @IsPositive()
+    totalBill: number;
+
+    @IsOptional()
+    @IsEnum(Currency)
+    currency: Currency;
+
+    @IsNotEmpty()
+    @IsString()
+    address: string;
+
+    @IsPositive()
+    beds: number;
+
+    @IsPositive()
+    baths: number;
+
+    @IsPositive()
+    rooms: number;
+}
