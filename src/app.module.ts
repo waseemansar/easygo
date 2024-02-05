c230d2b8589bab15918b2705a8018e65e651949e
waseemansar <waseemansar@outlook.com>
2024-02-05 15:09:34 +0400

    feat: added config module and applied validation on database enviorment variable using joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookingsModule } from './bookings/bookings.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-    imports: [PrismaModule, BookingsModule],
+    imports: [
+        PrismaModule,
+        BookingsModule,
+        ConfigModule.forRoot({
+            validationSchema: Joi.object({
+                DATABASE_URL: Joi.required(),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
