afa310e35057eb495de92f5023262bffb33b88b1
waseemansar <waseemansar@outlook.com>
2024-02-12 20:49:27 +0400

    refactor: added no auth decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Auth } from './auth/decorators/auth.decorator';
+import { AuthType } from './auth/enums/auth-type.enum';
 
+@Auth(AuthType.None)
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
